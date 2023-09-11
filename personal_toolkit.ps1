903ebc8e472365d4ef22c5b4b882997c3611f6c8
Maurice Götze <99mg24@web.de>
2023-09-11 20:24:06 +0200

    Persönliches Toolkit hinzugefügt

---
+++ personal_toolkit.ps1
@@ -0,0 +1,5 @@
+Import-Module ".\modules\utils\utils"
+Write-Host "Skript wird gestartet..." -ForegroundColor Green
+$path = (Get-Location).Path
+Write-Host "aktueller Pfad: $path" -ForegroundColor Green
+Write-Host "Test123"
