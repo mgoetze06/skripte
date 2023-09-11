960bb36bfe18d8b2dc61938b9b66c4c7dce2f408
Maurice Götze <99mg24@web.de>
2023-09-11 20:26:11 +0200

    import module angepasst

--- personal_toolkit.ps1
+++ personal_toolkit.ps1
@@ -1,4 +1,4 @@
-Import-Module ".\modules\utils\utils"
+Import-Module "C:\programs\powershell\skripte\modules\utils\utils"
 Write-Host "Skript wird gestartet..." -ForegroundColor Green
 $path = (Get-Location).Path
 Write-Host "aktueller Pfad: $path" -ForegroundColor Green
